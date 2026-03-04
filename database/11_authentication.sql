af6d93b9bbb2d002dd56946f065e830bf37c222b
James O'Shea <22768409+jamesoshea@users.noreply.github.com>
2026-03-04 19:41:20 +0100

    housekeeping

--- database/11_authentication.sql
+++ database/11_authentication.sql
@@ -1,5 +1,4 @@
 BEGIN;
-
 --------------------------------------------------------------------------------
 -- We use JSON Web Tokens to authenticate API requests. PostgREST
 -- cares specifically about a claim called role. When request
@@ -7,45 +6,37 @@ BEGIN;
 -- to the database role with that name for the duration of the
 -- HTTP request. If the client included no (or an invalid) JWT
 -- then PostgREST selects the role "anonymous".
-
 CREATE ROLE anonymous;
 CREATE ROLE web_user;
 GRANT anonymous, web_user TO authenticator;
-
 GRANT USAGE ON SCHEMA public TO anonymous, web_user;
-
 --------------------------------------------------------------------------------
 -- The user id is a string stored in postgrest.claims.sub. Let's
 -- wrap this in a nice function.
-
-CREATE FUNCTION current_user_id() RETURNS uuid
-STABLE
-LANGUAGE plpgsql
-AS $$
+CREATE FUNCTION current_user_id()
+    RETURNS uuid STABLE
+    LANGUAGE plpgsql
+    AS $$
 BEGIN
-	RETURN current_setting('postgrest.claims.userid');
+    RETURN current_setting('postgrest.claims.userid');
 EXCEPTION
-	-- handle unrecognized configuration parameter error
-	WHEN undefined_object THEN RETURN '';
+    -- handle unrecognized configuration parameter error
+    WHEN undefined_object THEN
+        RETURN '';
 END;
 $$;
-
 GRANT EXECUTE ON FUNCTION current_user_id() TO anonymous, web_user;
-
 --------------------------------------------------------------------------------
 -- We put things inside the hidden schema to hide
 -- them from public view. Certain public procs/views will
 -- refer to helpers and tables inside.
-
 CREATE SCHEMA hidden;
-
-CREATE TABLE hidden.users (
-	id            uuid      	not null default gen_random_uuid(),
-	"createdAt"   timestamp 	not null default now(),
-	"updatedAt"	  timestamp 	not null default now(),
-	username	  text 			not null,
-
-	constraint 	  users_id_key unique (id)
+CREATE TABLE hidden.users(
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
+    "createdAt" timestamp NOT NULL DEFAULT now(),
+    "updatedAt" timestamp NOT NULL DEFAULT now(),
+    username text NOT NULL,
+    CONSTRAINT users_id_key UNIQUE (id)
 );
-
 COMMIT;
+
--- database/20_schema.sql
+++ database/20_schema.sql
@@ -1,86 +1,58 @@
 BEGIN;
-
 --------------------------------------------------------------------------------
-
-CREATE TABLE routes (
-	id          		uuid   	  			primary key
-											not null
-											default gen_random_uuid(),
-	"userId"      		uuid        		not null
-	            				        	default current_user_id()
-	                          				references hidden.users (id),
-	"createdAt"     	timestamp   		not null
-	                          				default now(),
-	"brouterProfile"    character varying   not null,
-	points 				jsonb 				null
-											default '[]'::jsonb,
-	name 				text 				not null
-											default ''::text,
-
-  	constraint 			routes_id_key 		unique (id),
-  	constraint 			routes_name_check 	check ((length(name) <= 100))
+CREATE TABLE routes(
+    id uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    "userId" uuid NOT NULL DEFAULT current_user_id() REFERENCES hidden.users(id),
+    "createdAt" timestamp NOT NULL DEFAULT now(),
+    "brouterProfile" character varying NOT NULL,
+    points jsonb NULL DEFAULT '[]'::jsonb,
+    name text NOT NULL DEFAULT ''::text,
+    CONSTRAINT routes_id_key UNIQUE (id),
+    CONSTRAINT routes_name_check CHECK ((length(name) <= 100))
 );
-
 -- Author can insert and modify
-GRANT SELECT,
-	INSERT,
-	UPDATE,
-	DELETE
-ON TABLE routes TO web_user;
-
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE routes TO web_user;
 -- …but only of rows that they created themselves.
 ALTER TABLE routes ENABLE ROW LEVEL SECURITY;
-
-CREATE POLICY web_user_own_select ON routes FOR SELECT
-	USING ("userId" = current_user_id());
-
-CREATE POLICY web_user_own_create ON routes FOR INSERT
-	WITH CHECK ("userId" = current_user_id());
-
-CREATE POLICY web_user_own_update ON routes FOR UPDATE
-	WITH CHECK ("userId" = current_user_id());
-
-CREATE POLICY web_user_own_delete ON routes FOR DELETE
-	USING ("userId" = current_user_id());
-
+CREATE POLICY web_user_own_select ON routes
+    FOR SELECT
+        USING ("userId" = current_user_id());
+CREATE POLICY web_user_own_create ON routes
+    FOR INSERT
+        WITH CHECK ("userId" = current_user_id());
+CREATE POLICY web_user_own_update ON routes
+    FOR UPDATE
+        WITH CHECK ("userId" = current_user_id());
+CREATE POLICY web_user_own_delete ON routes
+    FOR DELETE
+        USING ("userId" = current_user_id());
 --------------------------------------------------------------------------------
-
-
-CREATE TABLE pois (
-  id 			uuid 						NOT NULL
-  											DEFAULT gen_random_uuid(),
-  "createdAt" 	timestamp with time zone	NOT NULL DEFAULT now(),
-  name 			character varying			NOT NULL,
-  "routeId" 	uuid 						NOT NULL
-											REFERENCES hidden.users (id),
-  coordinates 	json 						NOT NULL,
-  category 		character varying 			NOT NULL,
-  "userId" 		uuid 						NOT NULL
-	            				        	default current_user_id(),
-  CONSTRAINT 	pois_pkey 					PRIMARY KEY (id),
-  CONSTRAINT 	"pois_userId_fkey" 			FOREIGN KEY ("userId") REFERENCES hidden.users(id)
+CREATE TABLE pois(
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
+    "createdAt" timestamp with time zone NOT NULL DEFAULT now(),
+    name character varying NOT NULL,
+    "routeId" uuid NOT NULL REFERENCES hidden.users(id),
+    coordinates json NOT NULL,
+    category character varying NOT NULL,
+    "userId" uuid NOT NULL DEFAULT current_user_id(),
+    CONSTRAINT pois_pkey PRIMARY KEY (id),
+    CONSTRAINT "pois_userId_fkey" FOREIGN KEY ("userId") REFERENCES hidden.users(id)
 );
-
 -- Author can insert and modify name and description…
-GRANT SELECT,
-	INSERT,
-	UPDATE,
-	DELETE
-ON TABLE pois TO web_user;
-
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE pois TO web_user;
 -- …but only of rows that they created themselves.
 ALTER TABLE pois ENABLE ROW LEVEL SECURITY;
-
-CREATE POLICY web_user_own_select ON pois FOR SELECT
-	USING ("userId" = current_user_id());
-
-CREATE POLICY web_user_own_create ON pois FOR INSERT
-	WITH CHECK ("userId" = current_user_id());
-
-CREATE POLICY web_user_own_update ON pois FOR UPDATE
-	WITH CHECK ("userId" = current_user_id());
-
-CREATE POLICY web_user_own_delete ON pois FOR DELETE
-	USING ("userId" = current_user_id());
-
+CREATE POLICY web_user_own_select ON pois
+    FOR SELECT
+        USING ("userId" = current_user_id());
+CREATE POLICY web_user_own_create ON pois
+    FOR INSERT
+        WITH CHECK ("userId" = current_user_id());
+CREATE POLICY web_user_own_update ON pois
+    FOR UPDATE
+        WITH CHECK ("userId" = current_user_id());
+CREATE POLICY web_user_own_delete ON pois
+    FOR DELETE
+        USING ("userId" = current_user_id());
 COMMIT;
+

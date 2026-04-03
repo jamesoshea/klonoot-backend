3c4f39b50a2c294d22aec362a0db20770f9e26a1
James O'Shea <james@klim.eco>
2026-04-03 20:16:57 +0200

    it's cool infra

--- brouter/update-script.sh
+++ brouter/update-script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -d ./segments4 ] && [ -f ./segments4/W180_S85.rd5 ]; then
+  exit 0
+fi
+
 wget https://brouter.de/brouter/segments4 -q -O - | grep -Po '(?<=")(\w+\d+_\w+\d+.rd5)' | sed 's/^/https:\/\/brouter.de\/brouter\/segments4\//' > linksfinal.txt
 mkdir tmp
 cd tmp

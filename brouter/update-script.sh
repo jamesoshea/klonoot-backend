ab446212f516eb6210015aadae8ff15198293549
James O'Shea <22768409+jamesoshea@users.noreply.github.com>
2026-04-27 09:36:52 +0000

    client works, brouter not so much

--- brouter/update-script.sh
+++ brouter/update-script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# if [ -d ./segments4 ] && [ -f ./segments4/W180_S85.rd5 ]; then
-#   exit 0
-# fi
+if [ -d ./segments4 ] && [ -f ./segments4/E10_N45.rd5 ]; then
+  exit 0
+fi
 
-mkdir tmp
-wget -i ${PWD}/segments4/linksfinal.txt -P tmp
-rm -rf segments4
-mv tmp segments4
+mkdir segments4
+cd segments4
+wget https://brouter.de/brouter/segments4/E10_N45.rd5
+ls -a
